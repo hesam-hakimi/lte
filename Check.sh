6b8af25f0d0bf0b176c23471fe6cbdfb1aec2ae6
Hesam Hakimi <61381127+hesam-hakimi@users.noreply.github.com>
2026-09-21 09:09:40 -0400

    Add commands to list and check truststore.jks

--- Check.sh
+++ Check.sh
@@ -1,5 +1,2 @@
-date -u
-id
-ls -ld /etc/pki /etc/ssl
-find /etc /opt -maxdepth 4 -iname '*clue*' -print
-find /etc /opt -maxdepth 4 -iname '*.jks' -print
+ls -l /opt/springboot/security/truststore.jks
+keytool -list -keystore /opt/springboot/security/truststore.jks
