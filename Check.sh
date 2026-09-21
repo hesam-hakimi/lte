d9f8b0df594e044a645f2755d8cda69693b64f10
Hesam Hakimi <61381127+hesam-hakimi@users.noreply.github.com>
2026-09-21 07:19:58 -0400

    Remove unused curl command from Check.sh
    Removed curl command and related output handling.

--- Check.sh
+++ Check.sh
@@ -4,11 +4,3 @@ id
 ls -ld /etc/certs/
 ls -l /etc/certs/
 
-curl -q --silent --show-error --head \
-  --connect-timeout 10 --max-time 20 \
-  --output /dev/null \
-  --write-out 'http_code=%{http_code} http_connect=%{http_connect} ssl_verify_result=%{ssl_verify_result}\n' \
-  'https://penhubpat.td.com/aws/services/AwsService'
-
-clue_probe_rc=$?
-printf 'curl_exit=%s\n' "$clue_probe_rc"
