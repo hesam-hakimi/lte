7bc4697d822659facccc06aad3aa7627714aa901
Hesam Hakimi <61381127+hesam-hakimi@users.noreply.github.com>
2026-09-25 09:56:58 -0400

    Update Create.sh

--- Create.sh
+++ Create.sh
@@ -1,1 +1,2 @@
-curl -sS --proto '=https' --tlsv1.2 --cacert /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem -o /tmp/clue-nexus-repositories.json -w 'HTTP_STATUS=%{http_code}\n' 'https://rp.td.com/service/rest/v1/repositories'
+
+python3.12 -c 'import json; d=json.load(open("/tmp/clue-nexus-repositories.json")); r=[x for x in d if str(x.get("format","")).lower()=="pypi"]; print("\n".join("{} | type={} | {}".format(x.get("name"),x.get("type"),x.get("url")) for x in r) or "NO_PYPI_REPOSITORY")'
