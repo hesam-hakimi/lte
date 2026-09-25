a299af5d813f64a430ec87870f0553adcbaf9922
Hesam Hakimi <61381127+hesam-hakimi@users.noreply.github.com>
2026-09-25 08:57:12 -0400

    Replace chmod and mv with tar extraction command

--- Create.sh
+++ Create.sh
@@ -1,2 +1,2 @@
-chmod 600 ~/clue-bootstrap.sh
-mv ~/clue-bootstrap.sh ~/clue-bootstrap.sh.OLD_DO_NOT_RUN
+tar -xOzf /opt/clue/clue-code-1.0.5-SNAPSHOT.tar.gz \
+  deploy/clue_dev_run.sh | sed -n '1,260p'
