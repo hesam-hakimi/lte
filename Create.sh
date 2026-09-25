6f418c76972d078bd2b318ee66505224ba12ddb4
Hesam Hakimi <61381127+hesam-hakimi@users.noreply.github.com>
2026-09-25 12:10:35 -0400

    Update NAS host to use FQDN in Create.sh

--- Create.sh
+++ Create.sh
@@ -1,12 +1,7 @@
-NAS_HOST='Nasoc01sdprs01'
-NAS_SHARE='c01171_share_s_02'
+NAS_FQDN='<FQDN_FROM_WINDOWS>'
 
-getent hosts "$NAS_HOST"
+getent hosts "$NAS_FQDN"
 
-timeout 5 bash -c ">/dev/tcp/$NAS_HOST/445" \
+timeout 5 bash -c ">/dev/tcp/$NAS_FQDN/445" \
   && echo "PASS: SMB port 445 is reachable" \
   || echo "FAIL: SMB port 445 is not reachable"
-
-command -v smbclient || echo "smbclient is not installed"
-
-klist
